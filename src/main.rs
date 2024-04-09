d318ef68349be0d1aa165008660f9eee40e3d596
sho ochiai <ochi1020@gmail.com>
2024-04-09 20:55:52 +0900

    feat: finish basic implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    // The pattern to look for
+    pattern: String,
+    // The path to the file to read
+    path: std::path::PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line)
+        }
+    }
 }
